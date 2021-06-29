65995130e21a4542a0f80933f9d5296dd6158ecf
Euclides Vasconcelos <evascon@motorola.com>
2021-06-28 23:48:40 -0400

    Update functions heroku

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const port = 8080
-
 app.use(express.static(__dirname+ '/dist/front'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname+ '/dist/front/index.html')
+  res.sendFile(path.join(__dirname + '/dist/front/index.html'))
 })
 
-app.listen(port, ()=> {
-  console.log('Servidor iniciado na porta'+ port);
-})
+app.listen(process.env.PORT || 8080)
