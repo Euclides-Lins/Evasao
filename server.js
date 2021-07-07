a3e0b05f7f9c2c0d3170cf9f8fb7140f3c0ecaa3
Euclides Vasconcelos <evascon@motorola.com>
2021-07-07 02:24:16 -0400

    teste

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json')
-const middleware = jsonServer.defaults();
+//const middleware = jsonServer.defaults();
 
 //server.use(middleware);
 //server.use(router);
